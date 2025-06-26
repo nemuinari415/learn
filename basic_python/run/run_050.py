47a4d8cabdab238570b9622f169cc3c2dc079dd4
Yuki <lyukey0902@gmail.com>
2025-06-26 15:47:29 +0900

    --practice--

--- basic_python/run/run_050.py
+++ basic_python/run/run_050.py
@@ -7,12 +7,9 @@ logs = [
     {"user": "山田", "item": "りんご", "price": 120},
     {"user": "田中", "item": "りんご", "price": 120},
     {"user": "佐藤", "item": "バナナ", "price": 100},
-    {"user": "山田", "item": "みかん", "price": 80}
+    {"user": "山田", "item": "みかん", "price": 80},
 ]
 
 ph = PurchaseHistory(logs)
 ph.add_logs()
 ph.print_summary()
-
-# bash
-# PYTHONPATH=. python3 run/run_050.py
