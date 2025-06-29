eaaa9745f3607958313b673c490e5e19fd3c94ee
Yuki <lyukey0902@gmail.com>
2025-06-29 16:59:18 +0900

    --practice--

---
+++ 03_sql_basics/sql/s_011.sql
@@ -0,0 +1,7 @@
+-- 支出履歴を管理するテーブルを作成
+CREATE TABLE 支出履歴 (
+ID      INTEGER PRIMARY KEY,
+カテゴリ TEXT    DEFAULT '不明' NOT NULL,
+金額    INTEGER  DEFAULT 0 CHECK(金額 >= 0),
+メモ    VARCHAR(100) DEFAULT '不明' NOT NULL
+)
