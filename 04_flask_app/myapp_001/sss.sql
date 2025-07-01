083b75f2c3a20702c2925fa76681e72253669fe9
Yuki <lyukey0902@gmail.com>
2025-07-01 15:04:05 +0900

    --practice--

---
+++ 04_flask_app/myapp_001/sss.sql
@@ -0,0 +1,7 @@
+-- データベース編集用
+/*
+UPDATE profile
+   SET occupation = '騎兵隊長'
+ WHERE name = 'ガイア';
+
+SELECT * FROM profile; */
---
+++ 04_flask_app/run.py
@@ -0,0 +1,5 @@
+# myapp_001 起動用
+from myapp_001 import app
+
+if __name__ == "__main__":
+    app.run(debug=True)
