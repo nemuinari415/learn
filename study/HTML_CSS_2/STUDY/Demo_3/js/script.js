5d7fdcc572c4382f2a468303c0f91c38ff8da499
Yuki <lyukey0902@gmail.com>
2025-07-08 14:05:01 +0900

    --practice--

--- study/HTML_CSS_2/STUDY/Demo_3/js/script.js
+++ study/HTML_CSS_2/STUDY/Demo_3/js/script.js
@@ -1,22 +1,24 @@
-// 円グラフ
-
+/*
+PIE CHART
+================================================ */
 var pieData = {
-    labels: ['Webデザイナー', 'Webディベロッパー', 'サーバーエンジニア', '営業職'],
-    series: [14, 9, 8, 6]
+  labels: ['Webデザイナー', 'Webデベロッパー', 'サーバーエンジニア', '営業職'],
+  series: [14, 9, 8, 6]
 };
 
 var pieOptions = {
-    width: '100%',
-    height: '440px'
+  width: '100%',
+  height: '440px'
 };
 
 new Chartist.Pie('.pie-chart', pieData, pieOptions);
 
-// 棒グラフ
-
+/*
+BAR CHART
+================================================ */
 var barData = {
-    labels: ['2018年', '2019年', '2020年'],
-    series: [[10, 16, 29]]
+  labels: ['2018年', '2019年', '2020年'],
+  series: [[10, 16, 29]]
 };
 
 var barOptions = {
@@ -24,7 +26,7 @@ var barOptions = {
         offset: 60,
         scaleMinSpace: 50,
         labelInterpolationFnc: function(value) {
-            return value + ' 人'
+          return value + ' 人'
         }
     },
     width: '100%',
