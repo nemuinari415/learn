050acf192a25ecedeae1f339fde5e53d564cbfdd
Yuki <lyukey0902@gmail.com>
2025-07-10 13:02:13 +0900

    --practice--

---
+++ study/JavaScript_2/tutorial/app.js
@@ -0,0 +1,20 @@
+// alert("Hello World!");
+// console.log("Hello World!");
+// foo, bar, text
+/*
+const bar = () => {
+    const foo = 1 + 1;
+    console.log(foo);
+} */
+/*
+const colorDark = "#000";
+if(new Data().getHours() >= 0){
+    document.body.style.backgroundColor = colorDark;
+}
+if(user.setting.darkMode === true){
+    document.body.style.backgroundColor = colorDark;
+}
+*/
+// const = 再代入不可
+// let = 再代入可能
+// var = 再代入可能(主流ではない)
