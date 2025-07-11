ada8654de8a298ebae51bdeb3a9a170de5c631bd
Yuki <lyukey0902@gmail.com>
2025-07-11 13:27:30 +0900

    --practice--

--- study/JavaScript_2/tutorial/app.js
+++ study/JavaScript_2/tutorial/app.js
@@ -1,3 +1,4 @@
+// 1 =================================================
 // alert("Hello World!");
 // console.log("Hello World!");
 // foo, bar, text
@@ -18,3 +19,86 @@ if(user.setting.darkMode === true){
 // const = 再代入不可
 // let = 再代入可能
 // var = 再代入可能(主流ではない)
+// 2 =================================================
+/*
+const foo = "bar";
+const array = [1, "Hello", foo];
+
+console.log(array[1]); 
+*/
+/*
+const a = "ニャオハ";
+const b = "ホゲータ";
+const c = "クワッス";
+
+const fistPokemons = [a, b, c];
+console.log(fistPokemons[0],fistPokemons[1],fistPokemons[2]);
+*/
+/*
+const questions = [
+    "Aaa",
+    "Bbb",
+    "Ccc",
+];
+
+for (let index = 0; index < questions.length; index++) {
+    console.log(questions[index]);
+}
+*/
+/*
+let isLogin = false;
+if(!isLogin){ // && = and  || = or
+    alert("ログインしてください")
+} else {
+    console.log("ログイン成功中");
+}
+*/
+/*
+const myMoney = 1000;
+let isShipping = false;
+
+if(myMoney >= 500){
+    isShipping = true;
+}
+console.log(isShipping);
+*/
+/*
+if (new DataTransfer().getHours() > 20) {
+    document.body.style.backgroundColor = "#000";
+}
+*/
+/*
+function changeDarkMonde(obj) {
+    if (new Date().getHours() > obj.time) {
+        document.body.style.backgroundColor = obj.color;
+    }
+}
+
+changeDarkMonde({
+    time: 11,
+    color: "#333",
+})
+*/
+/*
+const sonicBoom = () => {
+    let damage = 20
+    return damage
+}
+console.log(sonicBoom()); // 20
+*/
+/*
+function metalBurst(lastDamage){
+    return lastDamage * 1.5;
+}
+
+console.log(metalBurst(10));
+*/
+function hornDrill(theirHp){
+    if (Math.random() <= 0.3){
+        return theirHp;
+    } else {
+        return theirHp = 0;
+    }
+}
+
+console.log(hornDrill(10));
